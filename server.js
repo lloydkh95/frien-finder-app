6209febebf712c09fca618745d62ec5a1b000b73
lloydkh95 <lloydkh95@gmail.com>
2019-09-12 17:30:41 -0400

    initial add with changes to be made

---
+++ server.js
@@ -0,0 +1,25 @@
+// pull in required dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// configure the express application
+var app = express();
+var PORT = process.env.PORT;
+
+// expose the public directory to acess CSS files
+app.use(express.static(path.join(__dirname, './app/public')));
+
+// add middleware for parsing incoming request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+// add the application routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(_dirname, './app/routing/htmlRoutes'))(app);
+
+// start listening on PORT
+app.listen(PORT, function() {
+    console.log('Friend Finder app is listening on PORT: ' + PORT);
+});
